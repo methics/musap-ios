30eea2e6a82c5696980f3a271f70336c1944deb0
tmub <teemu.manttari@gmail.com>
2024-03-25 16:13:42 +0200

    remove default impl of SscdSettings

--- Sources/extension/SscdSettings.swift
+++ Sources/extension/SscdSettings.swift
@@ -8,37 +8,7 @@
 import Foundation
 
 public protocol SscdSettings {
-    
-    func getSettings() -> [String:String]?
-    
     func getSetting(forKey key: String) -> String?
-    
+    func setSetting(key: String, value: String)
 }
 
-public extension SscdSettings {
-    
-    func getSetting(forKey key: String) -> String? {
-        
-        guard let settings = getSettings() else {
-            print("Getting setting failed, self.getSettings() was nil")
-            return nil
-        }
-        
-        return settings[key]
-    }
-    
-    func setSetting(key: String, value: String) -> Void {
-        print("setSetting: \(key) = \(value)")
-        if (self.getSettings() == nil) {
-            print("self.getSettings() was nil, return")
-            return
-        }
-        guard var settings = self.getSettings() else {
-            return
-        }
-        
-        settings[key] = value
-        print("Setting is set!")
-    }
-    
-}
